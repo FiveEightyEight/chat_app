53202d767f142b24a6563e817d0fafa147fa8395
Robert Abreu <robertabreu@pursuit.org>
2019-05-08 16:13:07 -0400

    Updates

---
+++ precache-manifest.376b6378477907754f7421494af34105.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1047e9367dd9209a7f2604f0d74e83",
+    "url": "/chat_app/index.html"
+  },
+  {
+    "revision": "edf1b1cc56a15fd59d9c",
+    "url": "/chat_app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "edf1b1cc56a15fd59d9c",
+    "url": "/chat_app/static/js/2.36b78275.chunk.js"
+  },
+  {
+    "revision": "f215aac2a43eb801f418",
+    "url": "/chat_app/static/js/main.3d1b5064.chunk.js"
+  },
+  {
+    "revision": "d0b6a141507207e42953",
+    "url": "/chat_app/static/js/runtime~main.8c90ab97.js"
+  }
+]);
